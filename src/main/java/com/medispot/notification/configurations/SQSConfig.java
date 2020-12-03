f45112db13912fa7a8bf7bf6aedb4f9e02a26b4f
AnishaA-git <anisha.agarwal1993@gmail.com>
2020-12-02 22:44:18 -0800

    change for build failure

--- src/main/java/com/medispot/notification/configurations/SQSConfig.java
+++ src/main/java/com/medispot/notification/configurations/SQSConfig.java
@@ -1,7 +1,9 @@
 package com.medispot.notification.configurations;
 
+import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
@@ -27,6 +29,16 @@ public class SQSConfig {
         converter.setSerializedPayloadClass(String.class);
         converter.setStrictContentTypeMatch(false);
         return converter;
+    }
+    
+    @Bean
+    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(
+        AmazonSQSAsync amazonSQSAsync
+    ) {
+      SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
+      factory.setAmazonSqs(amazonSQSAsync);
+      factory.setWaitTimeOut(1); // less than 10 sec
+      return factory;
     }
 }
 
